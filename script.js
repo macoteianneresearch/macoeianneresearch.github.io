2f5fe397a7afe147ce2202fd6f2a63d501b4e867
macoteianneresearch <macoteiannebernadettesedon@gmail.com>
2025-09-25 19:18:28 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showWarning() {
+  document.getElementById("warningPopup").classList.remove("hidden");
+}
+
+function closeWarning() {
+  document.getElementById("warningPopup").classList.add("hidden");
+}
